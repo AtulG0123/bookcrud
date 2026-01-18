d9511ba4d11eab21283b8fba23d57042e45fe95e
Atul Kumar <gupta2002atul@gmail.com>
2026-01-18 13:54:38 +0530

    testing only

--- server/index.js
+++ server/index.js
@@ -1,34 +1,29 @@
-const express=require('express')
-const app=express()
-const DBConnetion=require('./database')
-const bookRoutes=require('./routes/book.routes')
-const userRoutes= require('./routes/user.route')
-const {authMiddleware}= require('./middleware/auth.middleware')
-const cors= require('cors')
+const express = require("express");
+const app = express();
+const DBConnetion = require("./database");
+const bookRoutes = require("./routes/book.routes");
+const userRoutes = require("./routes/user.route");
+const { authMiddleware } = require("./middleware/auth.middleware");
+const cors = require("cors");
 
-app.use(express.json())
-// app.use(cors({
-//   origin:'*',
-//   methods:['GET', 'POST', 'PUT', 'DELETE'],
-//   credentials:true
-// }));
+app.use(express.json());
 app.use(
   cors({
-    origin: "https://bookcrud-hap2.vercel.app",
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   }),
 );
 
-
 DBConnetion();
 //routes
-app.get('/',(req,res)=>{
-  res.send("welcome to you first API")
-})
+app.get("/", (req, res) => {
+  res.send("welcome to you first API");
+});
 
 app.use("/Book", authMiddleware, bookRoutes);
-app.use('/User', userRoutes)
+app.use("/User", userRoutes);
 
-// app.listen(3001,(req,res)=>{
-//   console.log('server is running on port 3001')
-// })
+app.listen(3001, (req, res) => {
+  console.log("server is running on port 3001");
+});
