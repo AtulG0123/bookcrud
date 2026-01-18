055f57eb380da9a971ad71f701210271bf4639ce
Atul Kumar <gupta2002atul@gmail.com>
2026-01-18 13:11:29 +0530

    ok

--- server/database.js
+++ server/database.js
@@ -2,12 +2,16 @@ const mongoose = require('mongoose');
 
 const DBConnetion=async()=>{
   try {
-    mongoose.connect("mongodb://localhost:27017/BookShop")
-    .then(()=>{
-      console.log('Database connected successfully!')
-    }).catch((err)=>{
-      console.log('DB not connected')
-    })
+    mongoose
+      .connect(
+        "mongodb+srv://atul123:atul123@cluster0.sedk247.mongodb.net/?appName=Cluster0",
+      )
+      .then(() => {
+        console.log("Database connected successfully!");
+      })
+      .catch((err) => {
+        console.log("DB not connected");
+      });
   } catch (error) {
     return res.status(500).json({message:"internal error"})
   }
